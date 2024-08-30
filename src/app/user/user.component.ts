9b3883485b255b0efe21533003fb73bfb5316d49
James Wyatt <jamesrwyatt35@gmail.com>
2024-08-29 21:31:20 -0400

    typesrcipt interface

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,16 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
+// type User = {
+//   id:string; 
+//   avatar: string; 
+//   name: string
+// }
+
+interface User {
+  id:string; 
+  avatar: string; 
+  name: string
+}
 
 @Component({
   selector: 'app-user',
@@ -10,17 +20,19 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true }) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  // @Input({required: true }) id!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+  @Input({required: true}) user!: User;
+
   @Output() selectUser = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.selectUser.emit(this.id);
+    this.selectUser.emit(this.user.id);
   }
 
 }
